96a6dc160ca511fe45b1eaac2ee05f228c1115bd
Dragos Stanciu <stanciudragos1337@gmail.com>
2024-07-04 14:34:24 +0300

    GameBoard class creates a valid board
    (RED) GameBoard class creates a valid board
    
    (GREEN) GameBoard class creates a valid board

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,1 +1,19 @@
-export default class GameBoard {}
+export default class GameBoard {
+  #board = [];
+
+  get board() {
+    return this.#board;
+  }
+
+  createBoard() {
+    for (let i = 0; i < 10; i++) {
+      const x = [];
+      for (let j = 0; j < 10; j++) {
+        x.push({
+          isHit: false,
+        });
+      }
+      this.#board.push(x);
+    }
+  }
+}
--- src/gameboard/gameboard.test.js
+++ src/gameboard/gameboard.test.js
@@ -3,3 +3,19 @@ import GameBoard from "./gameboard";
 test("GameBoard class is initialized", () => {
   expect(new GameBoard()).not.toBeUndefined();
 });
+
+test("GameBoard class creates a valid board", () => {
+  const gameBoard = new GameBoard();
+  gameBoard.createBoard();
+  expect(gameBoard.board.length).toBe(10);
+
+  gameBoard.board.forEach((x) => {
+    expect(x.length).toBe(10);
+
+    x.forEach((y) => {
+      expect(y).toEqual({
+        isHit: false,
+      });
+    });
+  });
+});
