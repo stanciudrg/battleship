066d86532128040ad0c280bb664f17975d4fd94d
Dragos Stanciu <stanciudragos1337@gmail.com>
2024-07-11 12:18:43 +0300

    The controller creates the necessary players for the game
    (RED) The controller creates the necessary players for the game
    
    (GREEN) The controller creates the necessary players for the game

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,6 +3,8 @@ import GameBoard from "../gameboard/gameboard";
 import Player from "../player/player";
 
 export default class Controller {
+  #players = {};
+
   createGameBoard() {
     const board = new GameBoard();
     board.createBoard();
@@ -12,5 +14,14 @@ export default class Controller {
   createShip(length) {
     const ship = new Ship(length);
     return ship;
+  }
+
+  createPlayers() {
+    this.#players[1] = new Player(this.createGameBoard());
+    this.#players[2] = new Player(this.createGameBoard());
+  }
+
+  get players() {
+    return this.#players;
   }
 }
--- src/controller/controller.test.js
+++ src/controller/controller.test.js
@@ -1,6 +1,7 @@
 import Controller from "./controller";
 import GameBoard from "../gameboard/gameboard";
 import Ship from "../ship/ship";
+import Player from "../player/player";
 
 test("The controller creates a valid gameBoard", () => {
   const controller = new Controller();
@@ -10,4 +11,11 @@ test("The controller creates a valid gameBoard", () => {
 test("The controller creates a valid ship", () => {
   const controller = new Controller();
   expect(controller.createShip(4)).toBeInstanceOf(Ship);
+});
+
+test("The controller creates the necessary players for the game", () => {
+  const controller = new Controller();
+  controller.createPlayers();
+  expect(controller.players[1]).toBeInstanceOf(Player);
+  expect(controller.players[2]).toBeInstanceOf(Player);
 });
