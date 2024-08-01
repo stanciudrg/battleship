ae0aedc9d56c6a4c3c606256363fb805d1855fff
Dragos Stanciu <stanciudragos1337@gmail.com>
2024-08-01 11:46:57 +0300

    Add .vscode to .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -26,13 +26,11 @@ module.exports = {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ]
-          }
-        }
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
       {
         test: /\.css$/i,
